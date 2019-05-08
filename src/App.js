ff98d5759b6884d87a866628b646b8e915646a07
Robyn Blanchard <robynblanchard@notonthehighstreet.com>
2019-05-08 14:45:08 +0100

    Add grid template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { css } from 'styled-components';
+
+const gridWidth = '114rem';
+const gutterVertical = '8rem';
+const gutterHorizontal = '6rem';
+
+const Row = styled.div`
+  max-width: ${gridWidth};
+  margin: 0 auto;
+
+  &:not(:last-child) {
+    margin-bottom: ${gutterVertical};
+  }
+
+  &::after {
+    content: '';
+    display: table;
+    clear: both;
+  }
+`;
+
+const BaseCol = styled.div`
+  float: left;
+
+  &:not(:last-child) {
+    margin-right: ${gutterHorizontal};
+  }
+`;
+const Col = styled(BaseCol)`
+  background-color: blue;
+  ${({ numCols, width }) => {
+    return css`
+      width: calc(
+        ${width} * ((100% - ${numCols - 1} * ${gutterHorizontal}) / ${numCols}) +
+          ((${width} - 1) * ${gutterHorizontal})
+      );
+    `;
+  }}
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Row>
+        <Col width={1} numCols={2}>
+          col 1 of 2
+        </Col>
+        <Col width={1} numCols={2}>
+          col 1 of 2
+        </Col>
+      </Row>
+      <Row>
+        <Col width={1} numCols={3}>
+          col 1 of 3
+        </Col>
+        <Col width={2} numCols={3}>
+          col 2 of 3
+        </Col>
+      </Row>
+      <Row>
+        <Col width={1} numCols={3}>
+          col 1 of 3
+        </Col>
+        <Col width={1} numCols={3}>
+          col 1 of 3
+        </Col>
+        <Col width={1} numCols={3}>
+          col 1 of 3
+        </Col>
+      </Row>
+      <Row>
+        <Col width={1} numCols={4}>
+          col 1 of 4
+        </Col>
+        <Col width={1} numCols={4}>
+          col 1 of 4
+        </Col>
+        <Col width={1} numCols={4}>
+          col 1 of 4
+        </Col>
+        <Col width={1} numCols={4}>
+          col 1 of 4
+        </Col>
+      </Row>
+      <Row>
+        <Col width={1} numCols={4}>
+          col 1 of 4
+        </Col>
+        <Col width={2} numCols={4}>
+          col 2 of 4
+        </Col>
+        <Col width={1} numCols={4}>
+          col 1 of 4
+        </Col>
+      </Row>
     </div>
   );
 }
